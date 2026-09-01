2dea7ea230ee31f5784e3306a64622e8f820d0fd
Ayoub <ayoub18daoui@gmail.com>
2026-09-01 11:47:49 +0100

    Implement GET & POST class routes

--- src/controllers/classesController.ts
+++ src/controllers/classesController.ts
@@ -1,6 +1,8 @@
 import type { Request, Response } from "express";
 import { db } from "../config/db.js";
-import { classTable } from "../db/index.js";
+import { classTable, subject, user } from "../db/index.js";
+import { parsePositiveInt } from "../utils/index.js";
+import { and, count, desc, eq, getTableColumns, ilike, or } from "drizzle-orm";
 
 const createOneClass = async (req: Request, res: Response) => {
   try {
@@ -41,4 +43,117 @@ const createOneClass = async (req: Request, res: Response) => {
   }
 };
 
-export { createOneClass };
+const getAllClasses = async (req: Request, res: Response) => {
+  try {
+    const {
+      status,
+      search,
+      subjectName,
+      teacherName,
+      page = 1,
+      limit = 10,
+    } = req.query;
+
+    const currentPage = parsePositiveInt(page, 1);
+    const limitPerPage = Math.min(parsePositiveInt(limit, 10), 100);
+
+    // Calculate offset for SQL pagination
+    const offset = (currentPage - 1) * limitPerPage;
+
+    const filter = [];
+
+    // If search exists, filter by name
+    if (search) {
+      filter.push(ilike(classTable.name, `%${search}%`));
+    }
+
+    // If status exists, filter by status
+    if (status) {
+      filter.push(eq(classTable.status, status as "active" | "inactive"));
+    }
+
+    // If teacherName exists, filter by teacher name
+
+    if (teacherName) {
+      filter.push(ilike(user.name, `%${teacherName}%`));
+    }
+
+    // If subjectName exists, filter by subject name
+    if (subjectName) {
+      filter.push(ilike(subject.name, `%${subjectName}%`));
+    }
+
+    // Combinate two conditions with using AND
+    const whereClause = filter.length > 0 ? and(...filter) : undefined;
+
+    // Get count of classes
+    const countClasses = await db
+      .select({ count: count() })
+      .from(classTable)
+      .leftJoin(subject, eq(classTable.subjectId, subject.id))
+      .leftJoin(user, eq(classTable.teacherId, user.id))
+      .where(whereClause);
+
+    const totalCount = countClasses[0]?.count ?? 0;
+
+    const calssesList = await db
+      .select({
+        ...getTableColumns(classTable),
+        subject: { ...getTableColumns(subject) },
+        user: { ...getTableColumns(user) },
+      })
+      .from(classTable)
+      .leftJoin(subject, eq(classTable.subjectId, subject.id))
+      .leftJoin(user, eq(classTable.teacherId, user.id))
+      .where(whereClause)
+      .orderBy(desc(classTable.createdAt))
+      .limit(limitPerPage)
+      .offset(offset);
+
+    res.status(200).json({
+      data: calssesList,
+      pagination: {
+        page: currentPage,
+        limit: limitPerPage,
+        total: totalCount,
+        totalPages: Math.ceil(totalCount / limitPerPage),
+      },
+    });
+  } catch (error) {
+    console.log(`Can't GET classes : ${error}`);
+    res.status(500).json({ message: "Can't GET classes" });
+  }
+};
+
+const getOneClassById = async (req: Request, res: Response) => {
+  try {
+    const { classId } = req.params;
+
+    const numericClassId = Number(classId);
+    if (isNaN(numericClassId)) {
+      return res.status(400).json({ message: "Invalid class ID" });
+    }
+
+    const classResult = await db
+      .select({
+        ...getTableColumns(classTable),
+        teacher: { ...getTableColumns(user) },
+        subject: { ...getTableColumns(subject) },
+      })
+      .from(classTable)
+      .leftJoin(user, eq(classTable.teacherId, user.id))
+      .leftJoin(subject, eq(classTable.subjectId, subject.id))
+      .where(eq(classTable.id, numericClassId));
+
+    if (classResult.length === 0) {
+      return res.status(404).json({ message: "Class not found" });
+    }
+
+    res.status(200).json({ data: classResult[0] });
+  } catch (error) {
+    console.log(`Can't GET class : ${error}`);
+    res.status(500).json({ message: "Can't GET class" });
+  }
+};
+
+export { createOneClass, getAllClasses, getOneClassById };
--- src/routes/classRoute.ts
+++ src/routes/classRoute.ts
@@ -1,8 +1,14 @@
 import express from "express";
-import { createOneClass } from "../controllers/classesController.js";
+import {
+  createOneClass,
+  getAllClasses,
+  getOneClassById,
+} from "../controllers/classesController.js";
 
 const classRouter = express.Router();
 
+classRouter.get("/", getAllClasses);
 classRouter.post("/", createOneClass);
+classRouter.get("/:classId", getOneClassById);
 
 export default classRouter;
